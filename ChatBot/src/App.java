f8ec7099bc1716169afdab87f5a3be27680c4a60
iOzoki <isaac.adler@ufrpe.br>
2024-03-30 16:51:47 -0300

    Modificação do Scanner "escaneador" para "Escan"

--- ChatBot/src/App.java
+++ ChatBot/src/App.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner escaneador = new Scanner(System.in);
+        Scanner Escan = new Scanner(System.in);
         
         System.out.println("Olá, bem-vindo. Sou o bot Ozoki e irei lhe guiar no cadastro do WoW. Para começarmos, qual seu nome?");
-        String varNome = escaneador.nextLine();
+        String varNome = Escan.nextLine();
         
         System.out.println("É um prazer," + varNome + ". Qual sua idade?" );
-        int varIdade = escaneador .nextInt();
+        int varIdade = Escan .nextInt();
         if(varIdade >= 13) {
            System.out.println("Qual seu gênero?");
-         String varGenero = escaneador .nextLine();
+         String varGenero = Escan .nextLine();
         
          System.out.println("Com qual classe você deseja jogar?\n1.Guerreiro\n2.Caçador\n3.Ladino\n4.Sacerdote\n5.Paladino\n6.Xamã\n7.Conjurador\n8.Monge\n9.Bruxo\n10.Druida");
-         int varClasse = escaneador .nextInt();
+         int varClasse = Escan .nextInt();
 
          System.out.println("Seu cadastro está concluido! Seu nome é: " + varNome + " Sua idade: " + varIdade + " Seu genêro: " + varGenero + " E sua classe é: " + varClasse);
           
@@ -23,6 +23,6 @@ public class App {
         }
 
 
-        escaneador .close();
+        Escan .close();
     }
 }
