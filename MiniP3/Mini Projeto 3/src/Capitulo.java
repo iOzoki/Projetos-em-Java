291904e590680fe31e060a51e59aa0bb776c5e45
Isaac Adler <isaac.adler@ufrpe.br>
2024-04-26 10:42:30 -0300

    Classe Capitulo Criada

--- MiniP3/Mini Projeto 3/src/Capitulo.java
+++ MiniP3/Mini Projeto 3/src/Capitulo.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Capitulo {
     String nomeCap; 
     String texto;
     String escolha1;
     String escolha2;
+    
+    Scanner escolheuOpcao = new Scanner(System.in);
+
+    public Capitulo(String nomeCap, String texto){
+        this.nomeCap = nomeCap;
+        this.texto = texto;
+    }
 }
