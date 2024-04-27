e69a559a6d442052135842f018a26654a1aa1077
Isaac Adler <isaac.adler@ufrpe.br>
2024-04-27 11:28:18 -0300

    Classe capítulo mostrando informações com o método Mostrar() e utilizando o método Escolher() para tomar as decisões.

--- MiniP3/Mini Projeto 3/src/Capitulo.java
+++ MiniP3/Mini Projeto 3/src/Capitulo.java
@@ -3,20 +3,17 @@ import java.util.Scanner;
 public class Capitulo {
     String nomeCap; 
     String texto;
-    String escolha1;
-    String escolha2;
+    String escolha1 = "Opção 1";
+    String escolha2 = "Opção 2"; 
     Personagem personagem;
     int perderEnergia;
+    int escolhaUsuario; //Armazenar escolha do usúario 
     Scanner escolheuOpcao = new Scanner(System.in);
 
-    void Mostrar(){
-        System.out.println();
-
-    }
-
-    public Capitulo(String nomeCap, String texto){
+    public Capitulo(String nomeCap, String texto, Personagem personagem){
         this.nomeCap = nomeCap;
         this.texto = texto;
+        this.personagem = personagem;
     }
 
     public int Escolher(){
@@ -29,6 +26,13 @@ public class Capitulo {
             System.out.println("Opção inválida. Escolha novamente:");
             escolha = escolheuOpcao.nextInt(); 
         }
+        escolhaUsuario = escolha;
         return escolha; 
+    }
+
+    void Mostrar(){
+        System.out.println("Você está no capítulo: " + nomeCap + " O nome do seu personagem é: " + personagem.nome
+        + " E sua energia atualmente é: " + personagem.energia + " Suas escolhas até agora foram: " + (escolhaUsuario == 1 ? escolha1 : escolha2));
+
     }
 }
